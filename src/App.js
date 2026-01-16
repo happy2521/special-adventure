7e5f4d0ab0d8958907490fa22f299711feafc7b0
ManmathX <manmath2006n@gmail.com>
2026-01-16 17:49:53 +0530

    hemak=ntnsjcdf

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        hemant this idot and very idot
         </a>
       </header>
     </div>
